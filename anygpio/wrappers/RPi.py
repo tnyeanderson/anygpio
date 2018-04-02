cd55ba2d9316be12ac162b131f0e53f66a27dca7
Thomas Anderson <tnyeanderson@gmail.com>
2018-04-02 04:01:37 -0400

    Missed argument

--- anygpio/wrappers/RPi.py
+++ anygpio/wrappers/RPi.py
@@ -119,7 +119,7 @@ class RPi_PWMPin(RPi_Pin, anygpio.PWMPin):
 
 class GPIO(anygpio.GPIO):
 	# TEMPLATE: Change argument initial_value to the native_gpio.LOW value if needed
-	def setup_pin(self, number, name=None, action=anygpio.do_nothing, is_output=False, initial_value=0):
+	def setup_pin(self, number, name=None, action=anygpio.do_nothing, header=None, is_output=False, initial_value=0):
 		# Use this to initialize a pin
 		# Require the system to be set
 		self._require_system_set()
